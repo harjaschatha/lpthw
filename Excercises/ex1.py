c7deac88df45e3392f05d3b34bce39ad15a88be0
harjaschatha <harjaschatha@gmail.com>
2018-04-12 23:08:28 +0530

    Excercises 1 to 3

---
+++ Excercises/ex1.py
@@ -0,0 +1,7 @@
+print("Hello World")
+print("Hello again")
+print("Zed likes typing this")
+# print("He says it's fun")
+# print("Yay!! Printing, so he says")
+print("I'd much rather you 'not'")
+print('I said "Do not"')
---
+++ Excercises/ex2.py
@@ -0,0 +1,9 @@
+# A comment is so that you can read your program later
+# Anything afte the # is ignored by python
+
+print("I could have code like this") # and the comment shall be ignored
+
+# You can also use a comment to "Disable" or comment out code:
+# print("This won't run")
+
+print("This will run")
