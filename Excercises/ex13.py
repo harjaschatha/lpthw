973739dd9e06af0efe27fa863a9fc866fddd06c1
harjaschatha <harjaschatha@gmail.com>
2018-04-18 08:11:39 +0530

    Excercises 13 and 14

---
+++ Excercises/ex13.py
@@ -0,0 +1,11 @@
+from sys import argv
+
+script = argv
+
+print("The script is called:", script)
+first = input("please input the name of the first variable: ")
+print("Your first variable is:", first)
+second = input("please input the name of the second variable: ")
+print("Your second variable is:", second)
+third = input("please input the name of the third variable: ")
+print("Your third variable is:", third)
